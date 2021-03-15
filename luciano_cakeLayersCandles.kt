77b8f725eefb020f2447f936540bca13abf57678
kirlsoo <44961919+kirlsoo@users.noreply.github.com>
2021-03-16 02:16:40 +0800

    Create luciano_cakeLayersCandles.kt
    birthday cake code to always be the right size with theright number of candles for any age.

---
+++ luciano_cakeLayersCandles.kt
@@ -0,0 +1,36 @@
+fun main() {
+    var age = 24
+    var layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+}
+
+fun printCakeTop(age: Int){
+    repeat(age + 2) {  
+        print("=")
+    }
+    println()
+}
+
+fun printCakeCandles(age: Int) {
+    print (" ")
+    repeat(age) {  
+        print(",")
+    }  
+    println() // Print an empty line  
+    print(" ") // Print the inset of the candles on the cake
+    repeat(age) {  
+        print("|")
+    }  
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers) {
+        repeat(age + 2) {  
+            print("@")
+        }
+        println()
+    }
+}
